5058f95127a1d84d61b1f7088c6db00748220651
tzuting98914 <bee87914@gmail.com>
2020-08-25 20:22:50 +0800

    Initial Commit

---
+++ Knowhom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Knowhom
+//
+//  Created by MIS@NSYSU on 2020/8/25.
+//  Copyright © 2020年 knowhom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ KnowhomTests/KnowhomTests.swift
@@ -0,0 +1,34 @@
+//
+//  KnowhomTests.swift
+//  KnowhomTests
+//
+//  Created by MIS@NSYSU on 2020/8/25.
+//  Copyright © 2020年 knowhom. All rights reserved.
+//
+
+import XCTest
+@testable import Knowhom
+
+class KnowhomTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
